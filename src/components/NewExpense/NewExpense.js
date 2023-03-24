a29bf4e6719660e6b8960f43d94bfca58189812c
qwertyman30 <monish.789@gmail.com>
2023-03-24 20:31:47 +0100

    add component for filtering by year

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        props.onAddExpense(enteredExpenseData)
+    };
+
+    return (
+        <div className="new-expense">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        </div>
+    );
+};
+
+export default NewExpense;
